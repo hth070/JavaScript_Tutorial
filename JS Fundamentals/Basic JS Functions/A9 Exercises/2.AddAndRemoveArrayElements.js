901fd253b872471d0c4851a41ce1704c98110db9
Hristo Hristov <hth070@gmail.com>
2022-05-31 16:23:47 +0300

    adding some JS demo code again

---
+++ JS Fundamentals/Basic JS Functions/A9 Exercises/2.AddAndRemoveArrayElements.js
@@ -0,0 +1,25 @@
+function addOrRemove (arr) {
+    let resultArr=[];
+    let initialNumber = 1;
+    for (let i=0; i<arr.length; i++) {
+        if(arr[i]==='add') { 
+          resultArr.push(initialNumber);
+    }else if (arr[i] ==='remove') {
+          resultArr.pop();
+    }
+    initialNumber++; 
+}
+if (resultArr.length === 0) {
+    console.log("Empty");
+} else {
+    console.log(resultArr.join('\n'));
+}
+}
+
+addOrRemove(
+['add',
+'add',
+'remove',
+'add',
+'add']
+);
