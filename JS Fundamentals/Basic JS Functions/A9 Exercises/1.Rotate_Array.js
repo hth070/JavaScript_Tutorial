bcd7f82e1be5076c20c1f35504b0dd7192d4b604
Hristo Hristov <hth070@gmail.com>
2022-05-31 16:52:30 +0300

    1.Rotate_Array

---
+++ JS Fundamentals/Basic JS Functions/A9 Exercises/1.Rotate_Array.js
@@ -0,0 +1,16 @@
+function rotateArray(arr) { 
+    let rotations = +arr.pop();
+    for (let i=0; i<rotations; i++) {
+        arr.unshift(arr.pop());
+    }
+    console.log(arr.join(' '));
+  
+
+}
+
+rotateArray (['1',
+'2',
+'3',
+'4',
+'2'
+]);
