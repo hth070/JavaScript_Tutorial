db28071629609789b261b1538f2504a76d420248
Hristo Hristov <hth070@gmail.com>
2022-06-01 18:08:20 +0300

    Exercise that checks if string starts with given substring - true

---
+++ JS Fundamentals/Basic JS Functions/A11/A11.3.Check_If_String_Starts_With_Given_Substring.js
@@ -0,0 +1,8 @@
+function startsWith (ourString, partial) {
+    if(ourString.startsWith(partial)){
+        console.log(true);
+    }else {
+        console.log(false);
+    }
+}
+startsWith ('Marketing Fundamentals, starting 19/10/2016', 'Marketing Fundamentals, sta');
