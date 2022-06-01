6e18fbd5d0327449c910baa5cdbc09df9271ca8d
Hristo Hristov <hth070@gmail.com>
2022-06-01 14:08:55 +0300

    A10 exercise on vertical text display

---
+++ JS Fundamentals/Basic JS Functions/A10/VerticalText.js
@@ -0,0 +1,11 @@
+function stringDemo (str) {
+
+    for (let letter in str) {            //for in cycle we use to permabulate a string
+         console.log(str[letter]); 
+}
+
+}
+
+stringDemo('DXCRocks'); 
+
+//write DXCRocks vertically 
