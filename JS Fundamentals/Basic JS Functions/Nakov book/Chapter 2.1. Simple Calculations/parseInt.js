b0a62585cf5eb33046a0668a890232f4385b4445
Hristo Hristov <hth070@gmail.com>
2022-06-08 22:15:34 +0300

    Exercises Again

--- JS Fundamentals/Basic JS Functions/Nakov book/Chapter 2.1. Simple Calculations/parseInt.js
+++ JS Fundamentals/Basic JS Functions/Nakov book/Chapter 2.1. Simple Calculations/parseInt.js
@@ -1,7 +1,12 @@
 function calculateSquareArea([arg1]) {
-    let a = parseInt(arg1);
+    let a = parseInt(arg1);            
     let area = a * a;
     console.log('Square area = ' + area);
 }
 
 calculateSquareArea([3]);
+
+/*The parseInt function converts its first argument to a string, parses that string, 
+then returns an integer or NaN . 
+If not NaN , the return value will be the integer
+ that is the first argument taken as a number in the specified radix */
