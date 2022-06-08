3cb3cc5afaafae6344337f407b247c669dda4c55
Hristo Hristov <hth070@gmail.com>
2022-06-08 23:19:54 +0300

    Exercises 1

---
+++ JS Fundamentals/Basic JS Functions/Nakov book/Chapter 2.2. Simple Calculations – Exam Problems/PersonalInfo.js
@@ -0,0 +1,6 @@
+let firstName = "Stoqn";
+let lastName = "Dimitrov";
+let age = 40;
+let town = "Razgrad";
+console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+// You are Ivan Ivanov, a 19-years old person from Sofia.
