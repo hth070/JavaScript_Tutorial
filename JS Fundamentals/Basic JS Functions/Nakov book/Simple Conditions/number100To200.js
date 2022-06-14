7c0666a41dce84c941c98414e3c226ebd9974b57
Hristo Hristov <hth070@gmail.com>
2022-06-14 23:27:35 +0300

    Exercises

---
+++ JS Fundamentals/Basic JS Functions/Nakov book/Simple Conditions/number100To200.js
@@ -0,0 +1,14 @@
+let num = double.Parse(Console.ReadLine());
+function ot100Do200 ([])
+if (num > 1000) {
+	console.log("extremely fast");
+} else if (num <= 1000 && num > 150) {
+		console.log("ultra fast");
+} else if (num <= 150 && num > 50) {
+		console.log("fast");
+} else if (num <= 50 && num > 10) {
+	console.log("average");
+} else  {
+	console.log("slow");
+}
+
