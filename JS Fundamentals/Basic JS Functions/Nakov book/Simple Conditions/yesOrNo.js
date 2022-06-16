1ea22538e66aaf3d1d3a80e8c567220b4e22b185
Hristo Hristov <hth070@gmail.com>
2022-06-16 23:52:58 +0300

    Exercises

--- JS Fundamentals/Basic JS Functions/Nakov book/Simple Conditions/yesOrNo.js
+++ JS Fundamentals/Basic JS Functions/Nakov book/Simple Conditions/yesOrNo.js
@@ -11,3 +11,29 @@ function yesOrNo(input) {
 
   yesOrNo(['asdf', 'ASDF']);
   yesOrNo(['asdf', 'asd']);
+
+
+  /*
+
+  Problem: Equal Words
+Write a function that accepts two words as parameters and checks if they are the same. A comparison should be case-insensitive and the output should be either “yes” or “no”.
+
+Sample Input and Output
+Input	Output
+Hello
+Hello	yes
+SoftUni
+softuni	yes
+Soft
+Uni	no
+beer
+vodka	no
+HeLlO
+hELLo	yes
+Hints and Guidelines
+Before the comparison, both words should be in lower case, so that case (uppercase / lowercase) does not influence the result: word = word.toLowerCase().
+
+Testing in The Judge System
+Test your solution here: https://judge.softuni.org/Contests/Practice/Index/929#10.
+
+*/
