07d1835e42f17c705ff48263f903189c7a217ec5
Hristo Hristov <hth070@gmail.com>
2022-06-30 23:43:28 +0300

    More Exercise

---
+++ JS Fundamentals/Basic JS Functions/Nakov book/Nested Loops - Exam Problems/loopsByStep3.js
@@ -0,0 +1,10 @@
+function loopByStep3([arg1]){
+    let n = Number(arg1);
+   // console.log("n = " +n);
+   // console.log("The numbers are:");
+    for (let i = 1; i <= n; i+=3) {
+        console.log(i);
+    }
+}
+
+loopByStep3([100]);
