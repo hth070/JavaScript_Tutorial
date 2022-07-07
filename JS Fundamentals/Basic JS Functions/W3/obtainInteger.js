3c17812469f1ee631014da187625c60325224d31
Hristo Hristov <hth070@gmail.com>
2022-07-07 20:45:35 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/obtainInteger.js
@@ -0,0 +1,8 @@
+const randomIntegerInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomIntegerInRange(0, 1000));
+console.log(randomIntegerInRange(12, 75));
+console.log(randomIntegerInRange(-8, 5));
+console.log(randomIntegerInRange(-212, -7));
+
+
+// obtain a random int within the range 
