d4e073f4a51979ca53f052ebd042948e346c6c7e
Hristo Hristov <hth070@gmail.com>
2022-07-08 22:27:23 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/isItEven.js
@@ -0,0 +1,6 @@
+const evenChecker = num => num % 2 === 0;
+console.log(evenChecker(13));
+console.log(evenChecker(23));
+console.log(evenChecker(0));
+console.log(evenChecker(10));
+console.log(evenChecker(11));
--- JS Fundamentals/Basic JS Functions/W3/obtainDifference.js
+++ JS Fundamentals/Basic JS Functions/W3/obtainDifference.js
@@ -3,4 +3,8 @@ const difference = (a, b) => {
     return a.filter(x => !s.has(x));
   };
   console.log(difference([19, 12, 34], [11, 12, 45]));
-  console.log(difference([11, 12, 45],[11, 21, 34]));
+  console.log(difference([11, 12, 45],[11, 21, 34]));const isEven = num => num % 2 === 0;
+  console.log(isEven(3));
+  console.log(isEven(32));
+  console.log(isEven(1));
+  console.log(isEven(0));
