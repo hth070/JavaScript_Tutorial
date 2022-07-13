9becda3d98fdaff138ee5f898e9b189b90d84bff
Hristo Hristov <hth070@gmail.com>
2022-07-13 21:40:37 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/dataTypeReturn.js
@@ -0,0 +1,20 @@
+function dataTypeReturn(value)
+{
+var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
+console.log(dataTypeReturn(12));
+console.log(dataTypeReturn('w3resource'));
+console.log(dataTypeReturn(false));
