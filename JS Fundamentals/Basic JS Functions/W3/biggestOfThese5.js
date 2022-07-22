1d86648d4c0d1aa5015934ad1ef74ad40d8eb768
Hristo Hristov <hth070@gmail.com>
2022-07-22 23:44:46 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/biggestOfThese5.js
@@ -0,0 +1,25 @@
+v=-543;
+w=-23;
+x=345;
+y=53555;
+z=0;
+if (v>w && v>x && v>y && v>z)
+{
+    console.log(v);
+}
+else if (w>v && w>x && w>y && w>z)
+{
+    console.log(w);
+}
+else if (x>v && x>w && x>y && x>z)
+{
+    console.log(x);
+}
+else if (y>v && y>x && y>w && y>z)
+{
+    console.log(y);
+}
+else
+{
+    console.log(z);
+}
