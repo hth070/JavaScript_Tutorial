d76320378034bd42df1a10afa4118d00be1b21e6
Hristo Hristov <hth070@gmail.com>
2022-07-22 23:39:34 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/sortThese3Nums.js
@@ -0,0 +1,36 @@
+var a= 53;
+var b=-1234;
+var c= 0+2-1;
+if (a>b && a>c)
+{
+        if (b>c)
+        {
+            console.log(a + ", " + b + ", " +c);
+        }
+        else
+        {
+            console.log(a + ", " + c + ", " +b);
+        }
+}
+else if (b>a && b >c)
+{
+        if (a>c)
+        {
+             console.log(b + ", " + a + ", " +c);
+        }
+        else
+        {
+             console.log(b + ", " + c + ", " +a);
+        }
+}
+else if (c>a && c>b)
+{
+        if (a>b)
+        {
+            console.log(c + ", " + a + ", " +b);
+        }
+        else
+        {
+            console.log(c + ", " + b + ", " +a);
+        }
+}        
