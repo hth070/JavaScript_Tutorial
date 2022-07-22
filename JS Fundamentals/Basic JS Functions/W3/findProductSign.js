1d850556a932b7b117506640e3521bf69a583454
Hristo Hristov <hth070@gmail.com>
2022-07-22 23:26:37 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/findProductSign.js
@@ -0,0 +1,23 @@
+let a=0;
+let b=-213;
+let c=76;
+if (a>0 && b>0 && c>0)
+{
+       alert("The sign is +");
+}
+else if (a<0 && b<0 && c<0)
+        {
+          console.log("The sign is -");
+        }
+        else if (a>0 && b<0 && c<0)
+        {
+          console.log("The sign is +");
+        }
+        else if (a<0 && b>0 && c<0)
+        {
+          console.log("The sign is +");
+        }
+        else
+        {
+          console.log("The sign is -");
+        }
