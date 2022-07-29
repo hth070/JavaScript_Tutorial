34a1e7900c7a580349e6aeb00dfea19ab9b88b94
Hristo Hristov <hth070@gmail.com>
2022-07-29 23:22:47 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/1stCharacterUpperCaseOrNot.js
@@ -0,0 +1,14 @@
+function firstCharacterUpperCaseOrNot(str)
+{
+   regexp = /^[A-Z]/;
+   if (regexp.test(str))
+    {
+      console.log("String's first character is uppercase");
+    } 
+    else
+    {
+      console.log("String's first character is not uppercase");
+    }
+}
+firstCharacterUpperCaseOrNot('Abcd');
+firstCharacterUpperCaseOrNot('abcd');
