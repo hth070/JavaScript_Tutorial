19b8693ff7408f1ba92c9c744b629e7b1ed5695e
Hristo Hristov <hth070@gmail.com>
2022-08-09 23:03:25 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/roundIntValToNextMulOfFive.js
@@ -0,0 +1,8 @@
+function roundIntValToNextMulOfFive(num)
+{
+    return Math.ceil(num/5)*5;
+}
+
+console.log(roundIntValToNextMulOfFive(32));
+console.log(roundIntValToNextMulOfFive(137));
+console.log(roundIntValToNextMulOfFive(142));
