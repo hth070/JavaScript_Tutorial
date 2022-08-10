dda0e458347c91264462e57caed1b0d55b899d74
Hristo Hristov <hth070@gmail.com>
2022-08-10 19:12:06 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/roundNumStripZeroes.js
@@ -0,0 +1,9 @@
+//var a = -1.2330000;
+//var a = 0.00000099999989;
+//var a = 1.000000000;
+//var a = -4.55555;
+//var a = 5.0001000;
+var a = 1.1234000;
+
+var result = parseFloat(a.toFixed(4));
+console.log(result);
