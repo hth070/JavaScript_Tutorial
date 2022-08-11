25e858190d4204166d5b27a77a72777817c5cdac
Hristo Hristov <hth070@gmail.com>
2022-08-11 20:13:04 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/intToRoman.js
@@ -0,0 +1,16 @@
+function intToRoman(num) {
+    if (typeof num !== 'number') 
+    return false; 
+    
+    var digits = String(+num).split(""),
+    key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+    "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+    "","I","II","III","IV","V","VI","VII","VIII","IX"],
+    roman_num = "",
+    i = 3;
+    while (i--)
+    roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+    return Array(+digits.join("") + 1).join("M") + roman_num;
+    }
+    
+    console.log(intToRoman(27));
