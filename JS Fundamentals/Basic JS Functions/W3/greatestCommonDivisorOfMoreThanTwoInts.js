0bec3cd7e15c9b6babd19d3402b26fbe0e0f0ac8
Hristo Hristov <hth070@gmail.com>
2022-08-16 21:56:09 +0300

    More Exercsies

---
+++ JS Fundamentals/Basic JS Functions/W3/greatestCommonDivisorOfMoreThanTwoInts.js
@@ -0,0 +1,31 @@
+function greatestCommonDivisorOfMoreThanTwoInts(input) {
+    if (toString.call(input) !== "[object Array]")  
+          return  false;  
+    var len, a, b;
+      len = input.length;
+      if ( !len ) {
+          return null;
+      }
+      a = input[ 0 ];
+      for ( var i = 1; i < len; i++ ) {
+          b = input[ i ];
+          a = gcd_two_numbers( a, b );
+      }
+      return a;
+  }
+  
+  function gcd_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  console.log(greatestCommonDivisorOfMoreThanTwoInts([3,15,27]));
+  console.log(greatestCommonDivisorOfMoreThanTwoInts([5,10,15,25]));
+  
