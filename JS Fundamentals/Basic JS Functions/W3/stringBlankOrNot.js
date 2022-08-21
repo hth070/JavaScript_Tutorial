109fd4994a6b7809c99f5f6129bb969980d5adef
Hristo Hristov <hth070@gmail.com>
2022-08-21 23:09:49 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/stringBlankOrNot.js
@@ -0,0 +1,8 @@
+stringBlankOrNot =  function(input) {
+    if (input.length === 0)
+    return true;
+    else 
+    return false;
+  }
+console.log(stringBlankOrNot(''));
+console.log(stringBlankOrNot('abc'));
