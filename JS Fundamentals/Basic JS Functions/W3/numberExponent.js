26bc3e0867d45984e9de1d65edb9d7dfc9cf524d
Hristo Hristov <hth070@gmail.com>
2022-08-24 23:04:04 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/numberExponent.js
@@ -0,0 +1,13 @@
+var numberExponent = function(a, n) 
+{
+   if (n === 0) intArraySum
+   {
+    return 1;
+    }
+  else 
+  {
+    return a * numberExponent(a, n-1);
+  }
+};
+
+console.log(numberExponent(4, 2));
