7ee1880789e41f8784206508fe242a0b13f047c0
Hristo Hristov <hth070@gmail.com>
2022-08-28 19:06:06 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/weekStartDate.js
@@ -0,0 +1,11 @@
+function weekStartDate(date)
+  {
+    var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+  
+    return new Date(date.setDate(diff));
+ 
+  }
+
+dt = new Date(); 
+
+console.log(weekStartDate(dt).toString());
