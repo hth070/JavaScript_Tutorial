654d35f9a4627863ddcebd1d9ef05c0ca03005fd
Hristo Hristov <hth070@gmail.com>
2022-10-04 07:34:55 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/problem36.js
@@ -0,0 +1,13 @@
+const join = (arr, separator = ',', end = separator) =>
+  arr.reduce(
+    (acc, val, i) =>
+      i === arr.length - 2
+        ? acc + val + end
+        : i === arr.length - 1
+          ? acc + val
+          : acc + val + separator,
+    ''
+  );
+console.log(join(['pen', 'pineapple', 'apple', 'pen'], ',', '&'));
+console.log(join(['pen', 'pineapple', 'apple', 'pen'], ','));
+console.log(join(['pen', 'pineapple', 'apple', 'pen']));
