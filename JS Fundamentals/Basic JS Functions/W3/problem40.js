622482b3c232b2bdc34786e248a78396e11ae3d3
Hristo Hristov <hth070@gmail.com>
2022-10-06 23:22:29 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/problem40.js
@@ -0,0 +1,12 @@
+const createElement = str => {
+    const el = document.createElement('div');
+    el.innerHTML = str;
+    return el.firstElementChild;
+  };
+  
+  const el = createElement(
+    `<div class="container">
+      <p>Hello!</p>
+    </div>`
+  );
+  console.log(el.className); 
