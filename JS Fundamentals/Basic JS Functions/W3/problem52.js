46b6717c4171156fcb9b9e096fb6f756bef0d308
Hristo Hristov <hth070@gmail.com>
2022-10-10 22:41:55 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/problem52.js
@@ -0,0 +1,5 @@
+const flatten = (arr, depth = 1) =>
+  arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v), []);
+
+console.log(flatten([1, [2], 3, 4])); 
+console.log(flatten([1, [2, [3, [4, 5], 6], 7], 8], 2));
