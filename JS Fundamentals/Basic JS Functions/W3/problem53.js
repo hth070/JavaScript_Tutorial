77f1bae60894751d6bf614133fd72dbb03f3beb6
Hristo Hristov <hth070@gmail.com>
2022-10-12 22:41:19 +0300

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/problem53.js
@@ -0,0 +1,7 @@
+const pipeFunctions = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)));
+const add5 = x => x + 5;
+const multiply = (x, y) => x * y;
+const multiplyAndAdd5 = pipeFunctions(multiply, add5);
+
+console.log(multiplyAndAdd5(5, 2));
+console.log(multiplyAndAdd5(16, 2));
