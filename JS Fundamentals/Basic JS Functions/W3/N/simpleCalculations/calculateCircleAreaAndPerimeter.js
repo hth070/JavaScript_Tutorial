68262b7e5d444d7f84aab899a126c88eca70c806
Hristo Hristov <hth070@gmail.com>
2022-10-31 23:20:32 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/simpleCalculations/calculateCircleAreaAndPerimeter.js
@@ -0,0 +1,7 @@
+function calculateCircleAreaAndPerimeter(input1) {
+    let r = input1; 
+    console.log("Area  = " + Math.PI * r *r);
+    console.log("Perimeter  = " + 2 * Math.PI * r);
+}
+
+calculateCircleAreaAndPerimeter(10);
