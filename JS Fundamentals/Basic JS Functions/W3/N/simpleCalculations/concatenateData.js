ce07b3764f94e8c5a1d8a29741093956fda9ee3a
Hristo Hristov <hth070@gmail.com>
2022-10-31 23:22:17 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/simpleCalculations/concatenateData.js
@@ -0,0 +1,5 @@
+function printInfo ([firstName, lastName, age, town]) {
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old guy from ${town}`);
+}
+
+printInfo (["Hristo", "Hristov", 33, "Rousse"]);
