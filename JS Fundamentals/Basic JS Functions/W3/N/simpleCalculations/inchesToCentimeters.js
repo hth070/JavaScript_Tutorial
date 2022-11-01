db2f3461b66969ff50fe427afbae98e9896d2707
Hristo Hristov <hth070@gmail.com>
2022-11-01 22:49:26 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/simpleCalculations/inchesToCentimeters.js
@@ -0,0 +1,7 @@
+function convertInchesToCentimeters (arg1) {
+    let inches = parseFloat(arg1);
+    let centimeters = inches * 2.50;
+    console.log("The answer = " + centimeters);
+}
+
+convertInchesToCentimeters (2);
