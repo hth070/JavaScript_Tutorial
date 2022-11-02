490d41b62def77e81061c085ed050681a66fec34
Hristo Hristov <hth070@gmail.com>
2022-11-02 22:51:32 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/simple conditions/borisProblem.js
@@ -0,0 +1,14 @@
+function ednaFunkciq (a) {
+    if (a >= 1 && a <= 5 ) {
+        console.log(a);
+    }
+    else if (a<=20) {
+        console.log(a * 2);
+    }else{                            
+        console.log(a/2);
+    }
+}  
+
+ednaFunkciq (-1);
+ednaFunkciq (8);
+ednaFunkciq (50);
