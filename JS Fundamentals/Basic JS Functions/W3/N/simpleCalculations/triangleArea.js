13c0640f2620c19d47c1f4f9404e0572022cc00c
Hristo Hristov <hth070@gmail.com>
2022-11-02 22:48:19 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/simpleCalculations/triangleArea.js
@@ -0,0 +1,8 @@
+function calculateTriangleArea ([arg1, arg2]) {
+    let a = arg1;
+    let h = arg2;
+    let area = a * h / 2;
+    console.log(area.toFixed(2))
+}
+
+calculateTriangleArea ([8, 4]);
