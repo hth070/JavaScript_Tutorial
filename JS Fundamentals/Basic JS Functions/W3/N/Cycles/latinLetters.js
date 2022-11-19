15873f4e8249506cb0617f29b9d10afed002b7cc
Hristo Hristov <hth070@gmail.com>
2022-11-19 23:49:34 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/Cycles/latinLetters.js
@@ -0,0 +1,17 @@
+/*
+function latinLetters() {
+    for (i = 97; i <= 122; i++) {
+        console.log(String.fromCharCode(i));
+    }
+}
+
+latinLetters();
+*/
+
+
+
+function latinLetters() {
+    for (let ch = 'a'.charCodeAt(0); ch <='z'.charCodeAt(0); ch++) { 
+        console.log(String.fromCharCode(ch));
+        }
+    }
