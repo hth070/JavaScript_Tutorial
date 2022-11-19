2b28169476d2caeb57ce07ff8c402f4b4cb01451
Hristo Hristov <hth070@gmail.com>
2022-11-19 23:51:50 +0200

    More Exercises

---
+++ JS Fundamentals/Basic JS Functions/W3/N/Cycles/leftRightSum.js
@@ -0,0 +1,20 @@
+function leftRightSum(args) {
+    let n = Number(args[0]);
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for (var i = 1; i <= n; i++) {
+        leftSum += Number(args[i]);
+    }
+
+    for (var i = n + 1; i <= 2 * n; i++) {
+        rightSum += Number(args[i]);
+    }
+
+    if (leftSum == rightSum) {
+        console.log("Yes, sum = " + leftSum);
+    }
+    else {
+        console.log("No, diff = " + Math.abs(leftSum - rightSum));
+    }
+}s
