dd94c2d725a586a7a046848494a722d08cdc5221
Hristo Hristov <hth070@gmail.com>
2023-05-02 12:13:02 +0300

    USDtoBGN

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/USDtoBGN.js
@@ -0,0 +1,21 @@
+/*
+function usdToBGN([arg1]) {
+    let usd = Number(arg1);
+    let bgn = usd * 1.123456;
+    console.log(bgn.toFixed(2));
+}
+
+usdToBGN([1]);
+usdToBGN([2]);
+usdToBGN([3]);
+*/
+
+function usdToBGN ([arg1]) {
+    let usd = Number(arg1);
+    let bgn = usd * 1.123456;
+    console.log(bgn.toFixed(2));
+}
+
+usdToBGN([1]);
+usdToBGN([2]);
+usdToBGN([3]);
