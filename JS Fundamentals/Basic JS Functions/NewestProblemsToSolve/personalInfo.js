c25be98e3d8b29806e72426def0e4709b82399bd
Hristo Hristov <hth070@gmail.com>
2023-05-02 12:03:11 +0300

    personalInfo

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/personalInfo.js
@@ -0,0 +1,14 @@
+/*function personalInfo(firstName, lastName, age, town) {
+
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+}
+
+personalInfo(`Stoyan`, 'Yanev', 28, 'Varna');
+*/
+
+const firstName = "John";
+const lastName = "Doe";
+const age = 30;
+const town = "New York";
+
+console.log(`You are ${firstName} ${lastName}, a ${age}-years-old person from ${town}.`);
