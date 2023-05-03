1ab3b51a962113fdd62cb55c1c1779b0d1a1e451
Hristo Hristov <hth070@gmail.com>
2023-05-03 17:35:06 +0300

    dayOfTheWeek

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/weekDay.js
@@ -0,0 +1,33 @@
+function printDayOfWeek(day) {
+    switch(day) {
+      case 1:
+        console.log("Monday");
+        break;
+      case 2:
+        console.log("Tuesday");
+        break;
+      case 3:
+        console.log("Wednesday");
+        break;
+      case 4:
+        console.log("Thursday");
+        break;
+      case 5:
+        console.log("Friday");
+        break;
+      case 6:
+        console.log("Saturday");
+        break;
+      case 7:
+        console.log("Sunday");
+        break;
+      default:
+        console.log("Error");
+        break;
+    }
+  }
+
+  
+printDayOfWeek(1);    // prints "Monday"
+printDayOfWeek(5);    // prints "Friday"
+printDayOfWeek(10);   // prints "Error"
