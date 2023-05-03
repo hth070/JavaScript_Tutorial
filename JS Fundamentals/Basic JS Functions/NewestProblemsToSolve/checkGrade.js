e0ccce3f253da2054dc10dd022e626842eac723c
Hristo Hristov <hth070@gmail.com>
2023-05-03 17:03:58 +0300

    checkGrades

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/checkGrade.js
@@ -0,0 +1,19 @@
+function checkGrade(grade) {
+    if (grade >= 5.50) {
+      console.log("Excellent");
+    }
+  }
+
+checkGrade(6.0);  // prints "Excellent"
+checkGrade(5.3);  // does not print anything
+
+
+/*
+function checkGrade(grade) {
+  if (grade >= 5.50) {
+    console.log("Excellent");
+  } else {
+    console.log("Not excellent");
+  }
+}
+*/
