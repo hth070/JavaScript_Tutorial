2dc167ca362bd6aedaeda8b6be7cf7593699aa82
Hristo Hristov <hth070@gmail.com>
2023-05-03 18:13:36 +0300

    nestedConditions

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/nestedConditions.js
@@ -0,0 +1,10 @@
+if (condition1) {
+    console.log("condition1 valid");
+    if (condition2) {
+      console.log("condition2 valid");
+    } else {
+      console.log("condition2 not valid");
+    }
+  }
+  
+  
