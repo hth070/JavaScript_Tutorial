1ae0a21e28cfacc3ab07bf2a457b2deda1be5c1d
Hristo Hristov <hth070@gmail.com>
2023-05-03 17:14:42 +0300

    oddOrEven

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/oddOrEven.js
@@ -0,0 +1,11 @@
+function checkEvenOrOdd(num) {
+    if (num % 2 === 0) {
+      console.log("EVEN");
+    } else {
+      console.log("ODD");
+    }
+  }
+
+  
+checkEvenOrOdd(3);   // prints "ODD"
+checkEvenOrOdd(6);   // prints "EVEN"
