804f2e90270f52bf8575b8462f96dc29eaaf0af1
Hristo Hristov <hth070@gmail.com>
2023-05-05 11:28:50 +0300

    numsTo1000EndingOn7

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/numsTo1000EndingOn7.js
@@ -0,0 +1,11 @@
+function printNumbersEndingIn7() {
+    for (let i = 7; i <= 997; i++) {
+      if (i % 10 === 7) {
+        console.log(i);
+      }
+    }
+  }
+  
+  // Извикване на функцията
+  printNumbersEndingIn7();
+  
