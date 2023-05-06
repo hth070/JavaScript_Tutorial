df4d42e80a506a84ac6a11d6f3d14c81d7dc5d9e
Hristo Hristov <hth070@gmail.com>
2023-05-07 00:56:26 +0300

    arraySeparator

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/arrayStringSeparator.js
@@ -0,0 +1,9 @@
+let arr = [10, 20, 30];
+let separator = ':';
+let result = arr.join(separator);
+console.log(result); // "10:20:30"
+
+let words = ["one", "two"];
+separator = ' - ';
+result = words.join(separator);
+console.log(result); // "one - two"
