80890f17a594f17c75cf3bcb1455683ad337db48
Hristo Hristov <hth070@gmail.com>
2023-05-09 14:32:53 +0300

    slicingArrays

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/slicingArrays.js
@@ -0,0 +1,6 @@
+let	myArray	=	["one","two","three","four","five"];
+let	sliced	=	myArray.slice(2);
+console.log(myArray);	//
+["one","two","three","four","five"]
+console.log(sliced);	//	["three","four","five"]
+console.log(myArray.slice(2,4));	//	["three","four"]
