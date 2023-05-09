61f67c5628d4090d3fb2e4d03340ca24599acb70
Hristo Hristov <hth070@gmail.com>
2023-05-10 00:42:01 +0300

    mapMethod

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/mapFunction.js
@@ -0,0 +1,10 @@
+//▪ map()	–	creates	new	array	by	applying	a	function	to	
+//every	element
+let	myArr	=	['one', 'two', 'three', 'four'];
+let	lengths	=	myArr.map(x	=>	x.length);
+console.log(lengths);	//	[3,3,5,4]
+/*
+let	myArr	=	[1,	2,	3,	4];
+let	lengths	=	myArr.map(x	=>	x	*	2);
+console.log(lengths);	//	[2,4,6,8]
+*/
