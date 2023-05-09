e3986bc153bdf37726a8eba9d1d6feb4b791343c
Hristo Hristov <hth070@gmail.com>
2023-05-10 01:43:36 +0300

    sortingArraysExample

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/sortingArrays.js
@@ -0,0 +1,7 @@
+//Sorting Arrays – Example
+let	names	=	["Peter","George","Mary"];
+names.sort();	//	Default	behaviour	–	alphabetical	order
+console.log(names);	//	["George","Mary","Peter"]
+let	numbers	=	[20,	40,	10,	30,	100,	5];
+numbers.sort();	//	Unexpected	result	on	arrays	of	numbers!
+console.log(numbers);	//	[10,100,20,30,40,5]
