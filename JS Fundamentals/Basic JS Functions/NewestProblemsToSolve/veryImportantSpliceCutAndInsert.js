85fd898ac81365d3e531f3a52f65d5d9e626366a
Hristo Hristov <hth070@gmail.com>
2023-05-09 15:01:18 +0300

    veryImportantSliceCutAndInsert

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/veryImportantSpliceCutAndInsert.js
@@ -0,0 +1,15 @@
+/*let	nums	=	[5,	10,	15,	20,	25,	30];
+let	mid	=	nums.splice(2,	3);	//	start,	delete-count
+console.log(mid.join('|'));		//	15|20|25
+console.log(nums.join('|'));	//	5|10|30
+nums.splice(3,	2,	"twenty",	"twenty-five");
+console.log(nums.join('|'));	//	5|10|15|twenty|twenty-five|30 */
+
+
+
+let nums = [1, 2, 3, 4, 5, 6, 7];
+/*let mid = nums.splice(2, 4);
+console.log(mid.join('|'));
+console.log(nums.join('|')); */
+nums.splice (3,	2,	"twenty",	"twenty-five");
+console.log(nums.join('|'));
