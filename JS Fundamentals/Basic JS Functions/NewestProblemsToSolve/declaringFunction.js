d5bb2f0fee05e3215253e4c1670add002f2b21da
Hristo Hristov <hth070@gmail.com>
2023-05-11 21:23:31 +0300

    declaringFunction

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/declaringFunction.js
@@ -0,0 +1,14 @@
+/*
+function printText(text){
+		console.log(text);
+}
+*/
+let	printText	=	function(text){
+		console.log(text);
+}
+
+
+printText('hello');
+
+//Function	declaration	(recommended	way)
+//Function	expression	(useful	in	functional programming)
