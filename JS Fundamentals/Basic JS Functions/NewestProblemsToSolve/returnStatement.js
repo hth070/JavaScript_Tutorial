fc8a912a2e6918b3308945ce5012b0a1639157dc
Hristo Hristov <hth070@gmail.com>
2023-05-11 21:37:58 +0300

    returnStatement

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/returnStatement.js
@@ -0,0 +1,14 @@
+function	readFullName(firstName,	lastName)	{
+    return	firstName	+	" "	+	lastName;
+}
+const	fullName	=	readFullName("John","Dow");
+console.log(fullName)	//John	Dow
+
+
+
+/*
+The Return Statement
+▪ The	return	keyword	immediately	stops	the	
+function's	execution
+▪ Returns	the	specified	value	to	the	caller
+*/
