70172ee5a04700b32eaca90c41235c5416f0ef5f
Hristo Hristov <hth070@gmail.com>
2023-05-14 18:10:59 +0300

    explanationBuiltInMethodLibrary

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/Objects and Classes/explanationBuilt-InMethodLibrary.js
@@ -0,0 +1,12 @@
+//Built-in Method Library
+//Get	array	of	all	property	names	(keys)
+
+Object.keys(cat);	//	['name',	'age']
+
+//Get	array	with	of	all	property	values
+
+Object.values(cat);	//	['Tom',	5]
+
+//Get	and	array	of	all	properties	as	key-value	tuples
+
+Object.entries(cat);	//	[['name',	'Tom'],	['age',	5]]
