e9c3efae4939222d438162883702acba9fcfa764
Hristo Hristov <hth070@gmail.com>
2023-05-15 00:08:05 +0300

    valueTypes

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/Objects and Classes/valueTypes.js
@@ -0,0 +1,11 @@
+Value Types
+/*
+If	a	primitive	type	is	assigned	to	a	variable,	we	can	
+think	of	that	variable	as	containing	the	primitive	
+value
+*/
+
+let	a	=	10;	 	 	 let	c	=	a;
+let	b	=	'abc';	 	 let	d	=	b;
+//They	are	copied	by	value
+console.log(a,	b,	c,	d); //	a	=	10	b	=	'abc'	c	=	10	d	=	'abc'
