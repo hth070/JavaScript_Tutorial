1dcba641647bfcd0c6bc359daa193bd4f681676a
Hristo Hristov <hth070@gmail.com>
2023-05-14 17:25:32 +0300

    personInfoExercise

---
+++ JS Fundamentals/Basic JS Functions/NewestProblemsToSolve/personInfoExercise.js
@@ -0,0 +1,12 @@
+function createThePerson() {
+    let thePerson = {
+        firstname: "Ivan",
+        lastName: "Peshev",
+        age: 30
+    };
+
+    return thePerson;
+}
+
+let personObject = createThePerson();
+console.log(personObject);
